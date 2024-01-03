966a726021450f0a173f7f74eaa694583cf2d7eb
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2024-01-03 18:03:57 -0300

    configuring routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+import Home from './routes/Home/index.jsx'
+import NewPost from './routes/NewPost/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [{ path: "/", element: <Home /> }, { path: "/new", element: <NewPost /> } ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
