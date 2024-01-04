0498b17a3d7fb9ec626c985b3fca988aa6d88a5b
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2024-01-04 20:04:38 -0300

    home project

---
+++ src/routes/Home/index.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import './Home.css'
+import axios from "axios"
+import axiosInstance from "../../axios/config"
+
+const Home = () => {
+
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async () => {
+    try {
+      const res = await axiosInstance.get('/posts')
+
+      const data = res.data
+      setPosts(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+  return (
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? <p>Carregando</p> : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais...</Link>
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default Home
