0ad795d148dd0c8634f7264e76ae2df5b11b4087
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2024-01-07 01:22:04 -0300

    using context in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { useContext } from 'react'
+import { ThemeContext } from './context/ThemeContext'
+
 
 const App = () => {
+  const { isLightMode } = useContext(ThemeContext)
 
   return (
-    <div className='App'>
+    <div className={isLightMode?'dark_mode':'light_mode'}>
       <Navbar />
       <div className='container'>
         <Outlet />
